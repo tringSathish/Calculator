5ae690d7acc96a41d8a23ecd1c29f19659414aef
tringSubramaniyen <subramaniyen.s@tringapps.com>
2019-12-13 13:04:36 +0530

    Added calculator java class with junit

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,29 @@
+package com.Calculator;
+
+public class Calculator {
+
+    public long add(long first, long second) {
+
+        return first + second;
+
+    }
+
+    public long subtract(long first, long second) {
+
+        return first - second;
+
+    }
+
+    public long multiply(long first, long second) {
+
+        return first * second;
+
+    }
+
+    public long divide(long first, long second) {
+
+        return first / second;
+
+    }
+
+}
